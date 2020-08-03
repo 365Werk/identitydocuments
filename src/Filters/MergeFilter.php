f26efbf6a1f47510a3ad2c424d8d26ba136de28f
Hergen Dillema <hergen.dillema@gmail.com>
2020-08-03 16:24:11 +0200

    fix image merge

--- src/Filters/MergeFilter.php
+++ src/Filters/MergeFilter.php
@@ -37,11 +37,11 @@ class MergeFilter implements FilterInterface
         $base_img_y = $image->height();
         $add_img_x = $this->add_img->width();
         $add_img_y = $this->add_img->height();
-        $canvas_x = $base_img_x + $add_img_x;
-        $canvas_y = ($base_img_y > $add_img_y) ? $base_img_y : $add_img_y;
+        $canvas_y = $base_img_y + $add_img_y;
+        $canvas_x = ($base_img_x > $add_img_x) ? $base_img_x : $add_img_x;
         $canvas = Img::canvas($canvas_x, $canvas_y, '#ffffff');
         $canvas->insert($image, 'top-left');
-        $canvas->insert($this->add_img, 'top-left', $base_img_x);
+        $canvas->insert($this->add_img, 'top-left', 0, $base_img_y);
 
         return $canvas;
     }
