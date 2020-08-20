87b1536c0a1a5cc338e1ed97c678a9e52b84e8a7
Hergen Dillema <hergen.dillema@gmail.com>
2020-08-20 12:28:57 +0200

    1.3.2
    bugfix v2

--- src/IdentityDocumentsServiceProvider.php
+++ src/IdentityDocumentsServiceProvider.php
@@ -2,83 +2,38 @@
 
 namespace werk365\identitydocuments;
 
-use Illuminate\Filesystem\Filesystem;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
+
 
 class IdentityDocumentsServiceProvider extends ServiceProvider
 {
-    public function boot(Filesystem $filesystem)
+    /**
+     * The console commands.
+     *
+     * @var bool
+     */
+    protected $commands = [
+    ];
+
+    /**
+     * Bootstrap the application events.
+     * @throws BindingResolutionException
+     */
+    public function boot()
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'werk365');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'werk365');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        // Publishing is only necessary when using the CLI.
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
-
-        if (function_exists('config_path')) { // function not available and 'publish' not relevant in Lumen
-            $this->publishes([
-                __DIR__.'/../config/identitydocuments.php' => config_path('identitydocuments.php'),
-            ], 'config');
-        }
+        $this->publishes([
+            __DIR__.'/../config/identitydocuments.php' => config_path('identitydocuments.php'),
+        ]);
     }
 
     /**
-     * Register any package services.
-     *
-     * @return void
+     * Register the command.
      */
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/identitydocuments.php', 'identitydocuments');
 
-        // Register the service the package provides.
-        $this->app->singleton('identitydocuments', function ($app) {
-            return new IdentityDocuments;
-        });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['identitydocuments'];
-    }
-
-    /**
-     * Console-specific booting.
-     *
-     * @return void
-     */
-    protected function bootForConsole()
-    {
-        // Publishing the configuration file.
-        $this->publishes([
-            __DIR__.'/../config/identitydocuments.php' => config_path('identitydocuments.php'),
-        ], 'identitydocuments.config');
-
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/werk365'),
-        ], 'jwtAuthRoles.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/werk365'),
-        ], 'jwtAuthRoles.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/werk365'),
-        ], 'jwtAuthRoles.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
+        $this->commands($this->commands);
     }
 }
